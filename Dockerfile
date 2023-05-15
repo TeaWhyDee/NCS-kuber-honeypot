a4124591928b0a20a037e0595966dd53604f43cd
TeaWhyDee <daniel.555365@gmail.com>
2023-05-16 00:49:03 +0300

    add: working reporting

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,9 @@ RUN apt update
 RUN apt install libev-dev -y
 RUN apt install python3 -y
 RUN apt -y install python3-pip
+RUN apt -y install gawk
 RUN python3 -m pip install requests
  
-CMD /bin/bash -c "././mysql-honeypotd -s 5.6 -n mysql-replica -f | python3 catch_log.py"
+CMD /bin/bash -c "./mysql-honeypotd -s 5.6 -n mysql-replica -f 2>&1 | gawk '{print ; fflush()}' | xargs -L 1 -I '{}' python3 catch_log.py '{}'"
+# "./mysql-honeypotd -s 5.6 -n mysql-replica -f | gawk '{print; fflush()}' | xargs -L 1 python3 catch_log.py"
+# CMD ["./mysql-honeypotd", "-f"]
--- catch_log.py
+++ catch_log.py
@@ -1,17 +1,29 @@
 import re
 import sys
 import requests
+import os
 
 IP_REG = r'\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?):\d{1,5}\b'
 
 
 newfile = open("logs.log", "a+")
 
-for line in sys.stdin:
+# text = sys.stdin
+text = sys.argv
+# print(text)
+# print("!")
+
+for line in text:
     print(line.strip())
     matches = re.findall(IP_REG, line)
     if matches:
         for m in matches:
             newfile.write(m + '\n')
-        response = requests.post("https://ntfy.teawide.xyz/ncs", auth=("tmp", "tmp"), data=line.strip())
+        response = requests.post("https://ntfy.teawide.xyz/ncs",
+                                 auth=("tmp", "tmp"),
+                                 headers = {
+                                     'Priority': 'urgent',
+                                     'Tags': 'warning'
+                                 },
+                                 data=line.strip())
 
